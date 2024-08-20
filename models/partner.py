ec8faa3e2fb8ce8e34cbac930cf3fdc3c3775cd3
Antonio Queb <92013606+antonioqueb@users.noreply.github.com>
2024-08-20 08:47:00 -0600

    Up

--- models/partner.py
+++ models/partner.py
@@ -3,4 +3,8 @@ from odoo import models, fields
 class ResPartner(models.Model):
     _inherit = 'res.partner'
 
-    x_is_a_client = fields.Boolean(string="Ha sido cliente en el pasado?")
+    x_client_status = fields.Selection([
+        ('no', 'No ha sido cliente'),
+        ('yes', 'Cliente actual'),
+        ('former', 'Cliente anterior')
+    ], string="Estado del Cliente")
