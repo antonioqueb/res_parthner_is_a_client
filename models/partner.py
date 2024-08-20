e175527c03be74b3f32cf33bcf41644c2eb342d9
Antonio Queb <92013606+antonioqueb@users.noreply.github.com>
2024-08-20 09:13:28 -0600

    Up

--- models/partner.py
+++ models/partner.py
@@ -3,4 +3,4 @@ from odoo import models, fields
 class ResPartner(models.Model):
     _inherit = 'res.partner'
 
-    x_is_a_client = fields.Boolean(string="¿Ha sido cliente en el pasado?")
+    x_is_a_client = fields.Boolean(string="¿Es prioritario?")
